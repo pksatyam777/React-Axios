675b3f328cb51893db9d83828d4587a3ee7d54db
pksatyam777 <pksatyam777@gmail.com>
2021-03-03 13:51:59 +0530

    Updates

---
+++ precache-manifest.33a174f603510234188b2952aeb29f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0984ea69d2e20cf87cf1c38a5ea7fc8",
+    "url": "/React-Axios/index.html"
+  },
+  {
+    "revision": "2fc609c287f1b3d05a6e",
+    "url": "/React-Axios/static/css/main.ce0c819e.chunk.css"
+  },
+  {
+    "revision": "bda3faa2f39f299c88de",
+    "url": "/React-Axios/static/js/2.07e34e22.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/React-Axios/static/js/2.07e34e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc609c287f1b3d05a6e",
+    "url": "/React-Axios/static/js/main.0040f60f.chunk.js"
+  },
+  {
+    "revision": "1035b48ac098d5cfe225",
+    "url": "/React-Axios/static/js/runtime-main.7261648c.js"
+  }
+]);
